af5bbd96bd657fd9a9a970ef6810185079a67c20
Edward Osorno Muñoz <55111692+Edw10@users.noreply.github.com>
2020-02-21 21:09:28 -0500

    solucion completa primer punto

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 //Ejercicio
+/* 1.Hacer un programa en PHP que permita mostrar en pantalla la suma, resta, multiplicación, de dos números enteros
+almacenados en 2 variables diferentes. Conserve el siguiente formato de impresión: */
 
 echo("Taller 1 elaborado por Daniel Martinez<br>");
 echo("Punto 1");
@@ -16,6 +18,7 @@ $multiplicacion=$numero1*$numero2;
 $division=$numero1/$numero2;
 
 echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
-
-
+echo("<br>La resta de:".$numero1."-".$numero2."=".$resta);
+echo("<br>La multiplicacion de:".$numero1."*".$numero2."=".$multiplicacion);
+echo("<br>La division de:".$numero1."/".$numero2."=".$division);
 ?>
