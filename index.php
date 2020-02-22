65a61f9240dcc46ee54b4a24ac5a573ac21909c0
Edward Osorno Muñoz <55111692+Edw10@users.noreply.github.com>
2020-02-21 20:53:31 -0500

    primer taller, elaboracion principal

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-echo("Hola mundo");
+echo("TALLER 1 GENERALIDADES PHP- Eñabprado por Daniel");
+echo"<br>;
 
+$variable1 = 10;
+$variable2 = 15;
+
+echo(Ejercicio 1);
+
+suma = $variable1 + $variable2 
 
 
 
